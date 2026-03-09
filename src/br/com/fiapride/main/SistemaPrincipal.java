096afb28012006cf1cad24e6d1af95186df03e37
Bruna Marques e Queiroz <rm565648@fiap.com.br>
2026-03-09 14:58:20 -0300

    feat: adiciona dois novos métodos

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,23 +1,45 @@
 package br.com.fiapride.main;
- 
+
 import br.com.fiapride.model.Garrafa;
- 
+import java.util.Scanner;
+
 public class SistemaPrincipal {
- 
-	public static void main(String[] args) {
-		
-	Garrafa minhaGarrafa = new Garrafa();
-	minhaGarrafa.nivelVedação = (int) 10;
-	minhaGarrafa.Material = "Inox";
-	
-	Garrafa garrafaDaLaryssa = new Garrafa();
-	garrafaDaLaryssa.nivelVedação = (int) 4;
-	garrafaDaLaryssa.Material = "Plástico";
-	System.out.println("Minha garrafa é: " + minhaGarrafa.Material);
-	System.out.println("A garrafa da Laryssa é: " + garrafaDaLaryssa.Material);
-	System.out.println("Minha garrafa tem nível de vedação: " + minhaGarrafa.nivelVedação);
-	System.out.println("A garrafa da Laryssa tem nível de vedação: " + garrafaDaLaryssa.nivelVedação);
- 
-	}
- 
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+
+        Garrafa minhaGarrafa = new Garrafa();
+        minhaGarrafa.nivelVedacao = 10;
+        minhaGarrafa.material = "Inox"; 
+        minhaGarrafa.capacidadeMaxima = 750;
+        minhaGarrafa.volumeAtual = 0;
+
+        Garrafa garrafaDaLaryssa = new Garrafa();
+        garrafaDaLaryssa.nivelVedacao = 4;
+        garrafaDaLaryssa.material = "Plástico";
+
+        System.out.println("Minha garrafa é: " + minhaGarrafa.material);
+        System.out.println("A garrafa da Laryssa é: " + garrafaDaLaryssa.material);
+        System.out.println("Minha garrafa tem nível de vedação: " + minhaGarrafa.nivelVedacao);
+        System.out.println("A garrafa da Laryssa tem nível de vedação: " + garrafaDaLaryssa.nivelVedacao);
+
+        // teste dos novos  métodos
+        System.out.println("Utilização do método:");
+        
+        System.out.print("Quantos ml você quer colocar na garrafa? ");
+        int quantidadeParaEncher = leitor.nextInt(); 
+        minhaGarrafa.encher(quantidadeParaEncher); 
+
+        if (quantidadeParaEncher > minhaGarrafa.capacidadeMaxima) {
+            System.out.println("A garrafa transbordou, sistema encerrado.");
+            leitor.close();
+            return; //encerra se transbordar.
+        }
+        
+        System.out.print("\nQuantos ml você quer beber agora? ");
+        int quantidadeParaBeber = leitor.nextInt(); 
+        minhaGarrafa.beber(quantidadeParaBeber);  
+
+        leitor.close();
+    }
 }
--- src/br/com/fiapride/model/Garrafa.java
+++ src/br/com/fiapride/model/Garrafa.java
@@ -1,11 +1,39 @@
 package br.com.fiapride.model;
-public class Garrafa { 
-	
-    public String Material;
-    public String Formato;
-    public int nivelVedação;
+
+public class Garrafa {
+    public String material;
+    public String formato;
+    public int nivelVedacao;
     
+    //meus métodos  adicionados
+    public int capacidadeMaxima = 750;
+    public int volumeAtual = 0; //garrafa inicialmente
+
+    //Método 1: Apresenta volume com base na ação "encher".
+    public void encher(int quantidade) {
+        if (quantidade <= 0) {
+            System.out.println("A quantidade para encher deve ser maior que zero.");
+        } else if (volumeAtual + quantidade > capacidadeMaxima) {
+            System.out.println("Capacidade máxima é " + capacidadeMaxima + "ml.");
+        } else {
+            volumeAtual += quantidade;
+            System.out.println("Você encheu " + quantidade + "ml. Volume atual: " + volumeAtual + "ml.");
+        }
+    }
+
+    // Método 2: Altera volume atual com base na ação "beber".
+    public void beber(int quantidade) {
+        if (quantidade <= 0) {
+            System.out.println("A quantidade para beber deve ser maior que zero.");
+        } else if (quantidade > volumeAtual) {
+            System.out.println("Não há líquido suficiente na garrafa para beber" + quantidade + "ml.");
+        } else {
+            volumeAtual -= quantidade;
+            System.out.println("Você bebeu " + quantidade + "ml. Volume atual: " + volumeAtual + "ml.");
+        }
+    }
 }
 
-//Cor e material sendo String: "Cilíndrico", "Prisma", "Inox", "Plástico"... Porque são textos no código.
+//Formato  e material sendo String: "Cilíndrico", "Prisma", "Inox", "Plástico"... Porque são textos no código.
 //Vedação sendo int porque geralmente calculamos nível em números inteiros" 750ml, 200ml..."
+//Adição de mais dois métodos "beber" e "encher", que implicam em mudança nos atributos.
